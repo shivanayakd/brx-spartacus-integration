7fa1f0320b1a134ba3fcaa14b263e5e689103287
Shiva.Dharavath <shiva.dharavath@bloomreach.com>
2021-10-13 20:58:19 +0530

    ENT-3594 - Facets few fixes

--- js-storefront/brx-spartacus/src/app/spartacus/brx/adapters/brx-product-search-adapter.ts
+++ js-storefront/brx-spartacus/src/app/spartacus/brx/adapters/brx-product-search-adapter.ts
@@ -1,6 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ConverterService, Product, ProductSearchAdapter, ProductSearchPage, PRODUCT_NORMALIZER, PRODUCT_SEARCH_PAGE_NORMALIZER, PRODUCT_SUGGESTION_NORMALIZER, SearchConfig, Suggestion } from '@spartacus/core';
+import {
+  ConverterService,
+  Product,
+  ProductSearchAdapter,
+  ProductSearchPage,
+  PRODUCT_NORMALIZER,
+  PRODUCT_SEARCH_PAGE_NORMALIZER,
+  PRODUCT_SUGGESTION_NORMALIZER,
+  SearchConfig,
+  Suggestion,
+} from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { map, pluck, take } from 'rxjs/operators';
 import { BrxSearchConfig } from '../../../brx/models/brx-search-config.model';
@@ -12,62 +22,59 @@ const DEFAULT_SEARCH_CONFIG: BrxSearchConfig = {
 };
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class BrxProductSearchAdapter implements ProductSearchAdapter  {
-
+export class BrxProductSearchAdapter implements ProductSearchAdapter {
   constructor(
     protected http: HttpClient,
     protected converter: ConverterService,
     protected brxEndpointService: BrxEndpointService
-  ) { }
+  ) {}
 
   search(
     query: string,
-    searchConfig: BrxSearchConfig = DEFAULT_SEARCH_CONFIG,
+    searchConfig: BrxSearchConfig = DEFAULT_SEARCH_CONFIG
   ): Observable<ProductSearchPage> {
-    console.log('[** Product Search Adapter - Custom Brx API Req---]', query, searchConfig);
+    console.log(
+      '[** Product Search Adapter - Custom Brx API Req---]',
+      query,
+      searchConfig
+    );
     if (searchConfig.type === 'suggestion') {
-      return this.http
-        .get(this.getSuggestionEndpoint(query))
-        .pipe(
-          pluck<any, Object[]>('response', 'products'),
-          map((model: Object[]) => model.slice(0, searchConfig.pageSize)),
-          this.converter.pipeableMany(PRODUCT_NORMALIZER),
-          map<Product[], ProductSearchPage>((model: Product[]) => ({
-            products: model,
-          })),
-        );
+      return this.http.get(this.getSuggestionEndpoint(query)).pipe(
+        pluck<any, Object[]>('response', 'products'),
+        map((model: Object[]) => model.slice(0, searchConfig.pageSize)),
+        this.converter.pipeableMany(PRODUCT_NORMALIZER),
+        map<Product[], ProductSearchPage>((model: Product[]) => ({
+          products: model,
+        }))
+      );
     }
-    const searchQuery = query.split(':')[0];
+    const searchQuery = query ? query.split(':')[0] : '';
     const url = this.getSearchEndpoint(searchQuery, searchConfig);
 
-    return this.http
-      .get(url)
-      .pipe(
-        map((response: any) => { 
-          return {
-            ...response,
-            query,
-            url,
-            sort: searchConfig.sort,
-          }
-        }),
-        this.converter.pipeable(PRODUCT_SEARCH_PAGE_NORMALIZER)
-      );
+    return this.http.get(url).pipe(
+      map((response: any) => {
+        return {
+          ...response,
+          query,
+          url,
+          sort: searchConfig.sort,
+        };
+      }),
+      this.converter.pipeable(PRODUCT_SEARCH_PAGE_NORMALIZER)
+    );
   }
 
   loadSuggestions(
     term: string,
     pageSize: number = 3
   ): Observable<Suggestion[]> {
-    return this.http
-      .get(this.getSuggestionEndpoint(term))
-      .pipe(
-        pluck<any, Object[]>('response', 'suggestions'),
-        map((model: Object[]) => model.slice(0, pageSize)),
-        this.converter.pipeableMany(PRODUCT_SUGGESTION_NORMALIZER)
-      );
+    return this.http.get(this.getSuggestionEndpoint(term)).pipe(
+      pluck<any, Object[]>('response', 'suggestions'),
+      map((model: Object[]) => model.slice(0, pageSize)),
+      this.converter.pipeableMany(PRODUCT_SUGGESTION_NORMALIZER)
+    );
   }
 
   protected getSearchEndpoint(
@@ -77,10 +84,7 @@ export class BrxProductSearchAdapter implements ProductSearchAdapter  {
     return this.brxEndpointService.buildSearchUrl(query, searchConfig);
   }
 
-  protected getSuggestionEndpoint(
-    term: string
-  ): string {
+  protected getSuggestionEndpoint(term: string): string {
     return this.brxEndpointService.buildSuggestionUrl(term);
   }
-
 }
--- js-storefront/brx-spartacus/src/app/spartacus/brx/normalizers/brx-product-search-normalizer.ts
+++ js-storefront/brx-spartacus/src/app/spartacus/brx/normalizers/brx-product-search-normalizer.ts
@@ -87,60 +87,70 @@ export class BrxProductSearchNormalizer
 
     const sort = source.sort ?? 'relevance';
     const queryList = source.query?.split(':');
-    const query: string = queryList?.length < 2 ? `${source.query}:${sort}` : source.query;
-    target.facets = Object.entries(source.facet_counts?.facet_fields ?? [])
-      .map((facetEntry) => {
+    const query: string =
+      queryList?.length < 2 ? `${source.query}:${sort}` : source.query;
+    if (query) {
+      target.facets = Object.entries(
+        source.facet_counts?.facet_fields ?? []
+      ).map((facetEntry) => {
         const facetName = facetEntry[0];
-        const values = (facetEntry[1] as BloomreachFacet[])
-          .filter((value) => {
-            const facetValue = value.cat_name ?? value.name;
-            return query.indexOf(`${facetName}:${facetValue}`) < 0
-          })
-          .map((value) => {
-            const facetValue = value.cat_name ?? value.name;
-            return {
-              count: value.count,
-              name: facetValue,
-              query : {
-                query : {
-                  value : `${query}:${facetName}:${facetValue}`,
+        const values =
+          (facetEntry[1] as BloomreachFacet[])
+            .filter((value) => {
+              const facetValue = value.cat_name ?? value.name;
+              return query.indexOf(`${facetName}:${facetValue}`) < 0;
+            })
+            .map((value) => {
+              const facetValue = value.cat_name ?? value.name;
+              return {
+                count: value.count,
+                name: facetValue,
+                query: {
+                  query: {
+                    value: `${query}:${facetName}:${facetValue}`,
+                  },
+                  url: `/search?q=${query}%3A${facetName}%2C+${encodeURIComponent(
+                    facetValue
+                  )}`,
                 },
-                url : `/search?q=${query}%3A${facetName}%2C+${encodeURIComponent(facetValue)}`,
-              },
-              selected: false
-          }
-        }) ?? [];
+                selected: false,
+              };
+            }) ?? [];
 
         return {
           category: false,
           multiSelect: true,
           name: facetName,
           values,
-          visible: true
-        }
+          visible: true,
+        };
       });
 
-    target.breadcrumbs = [];
-    for (let i = 2; i < queryList.length; i=i+2) {
-      const removeQuery = query.replace(`:${queryList[i]}:${queryList[i+1]}`, '');
-      target.breadcrumbs?.push({
-        facetCode : queryList[i],
-        facetName : queryList[i],
-        facetValueCode : queryList[i+1],
-        facetValueName : queryList[i+1],
-        removeQuery : {
-          query : {
-              value : removeQuery,
+      target.breadcrumbs = [];
+      for (let i = 2; i < queryList.length; i = i + 2) {
+        const removeQuery = query.replace(
+          `:${queryList[i]}:${queryList[i + 1]}`,
+          ''
+        );
+        target.breadcrumbs?.push({
+          facetCode: queryList[i],
+          facetName: queryList[i],
+          facetValueCode: queryList[i + 1],
+          facetValueName: queryList[i + 1],
+          removeQuery: {
+            query: {
+              value: removeQuery,
+            },
+            url: `/search?q=${encodeURIComponent(removeQuery)}`,
           },
-          url : `/search?q=${encodeURIComponent(removeQuery)}`
-        }
-      });
-    }
+        });
+      }
 
-    target.currentQuery = {
-      query: {
-        value: `${query}:`
-      },
+      target.currentQuery = {
+        query: {
+          value: `${query}:`,
+        },
+      };
     }
     // Convert the Source to target
     target.sorts = src.sorts;
